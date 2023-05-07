c83d39d599a954dbfa9f0afb6692cc0fa69f562d
VB <vikram.bhagavatula@gmail.com>
2023-05-07 15:48:07 -0700

    Cleaned up unused code

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,7 +2,6 @@ import nibabel as nib
 import numpy as np
 import os
 from assets import prob_map
-import logging
 
 mask = nib.load(prob_map['background']).get_fdata()
 
@@ -43,30 +42,4 @@ def skullstrip(scan):
     nii_image = nib.Nifti1Image(scan_data.astype(np.float32), affine=None, header=header)
     nii_mask_image = nib.Nifti1Image(mask_ret.astype(np.float32), affine=None, header=header)
 
-    logging.info(f"nii_image.shape = {nii_image.shape}")
-    logging.info(f"nii_image is empty: {not np.any(nii_image)}")
-    logging.info(f"nii_mask_image.shape = {nii_mask_image.shape}")
-
     return nii_image, nii_mask_image
-
-#def skullstrip(scan):
-#    """ Original implementation
-#    """
-
-#    scan_data = scan.get_fdata().copy()
-    
-#    # after conversion apply MNI mask
-#    scan_data[np.where(mask >= 0.3)] = 0
-
-#    #generate binary mask
-#    mask_ret = scan_data.copy()
-#    mask_ret[np.where(mask >= 0.3)] = 0
-#    mask_ret[np.where(mask_ret <= 0)] = 0
-#    mask_ret[np.where(mask_ret > 0)] = 1
-
-#    header = scan.header
-
-#    nii_image = nib.Nifti1Image(scan_data.astype(np.float32), affine=None, header=header)
-#    nii_mask_image = nib.Nifti1Image(mask_ret.astype(np.float32), affine=None, header=header)
-
-#    return nii_image, nii_mask_image
